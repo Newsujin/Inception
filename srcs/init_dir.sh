52b8d066cf1756e9bfffa20ad8d4f10bea6f1553
sujin Park <chemd78@naver.com>
2024-10-18 16:57:48 +0900

    :sparkles: feat: init_dir.sh 파일 추가

---
+++ srcs/init_dir.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ENV_FILE="srcs/.env"
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    # macOS
+    BASE_DIR="/Users/sujin/data"
+else
+    # Linux
+    BASE_DIR="/home/$USER/data"
+fi
+
+if [ "$1" == "--delete" ]; then
+    echo "Deleting volume..."
+    rm -rf "$BASE_DIR"
+    echo "Delete COMPLETE!!!"
+
+    if [ -f "$ENV_FILE" ]; then
+        sed -i '' '/^DATA_PATH=/d' "$ENV_FILE"
+    else
+        echo ".env file not found. Skipping DATA_PATH removal."
+    fi
+    exit 0
+fi
+
+if [ ! -d "$BASE_DIR" ]; then
+    echo "Add volumes..."
+	mkdir -p $BASE_DIR/wordpress/
+	mkdir -p $BASE_DIR/mariadb/
+    echo "Add COMPLETE!!!"
+fi
+
+if ! grep -q "DATA_PATH=" srcs/.env; then
+	if [ -s srcs/.env ] && [ "$(tail -c 1 srcs/.env | wc -l)" -eq 0 ]; then
+    echo "" >> srcs/.env
+	fi
+    echo "DATA_PATH=$BASE_DIR" >> srcs/.env
+fi
